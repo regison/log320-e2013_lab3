f742bd1540e44211014e50e8f0db5cf72a41b8d6
regison <reginald.augustin@gmail.com>
2013-06-25 08:52:53 -0700

    [RAU] ajout des méthodes (partielles) pour la validation et génération des moves

--- src/utils/MoveGenerator.java
+++ src/utils/MoveGenerator.java
@@ -1,5 +1,41 @@
 package utils;
 
-public class MoveGenerator {
+import java.util.ArrayList;
+
+import model.Move;
+import model.Piece;
+import interfaces.IMoveGenerator;
+
+public class MoveGenerator implements IMoveGenerator {
+
+	@Override
+	public boolean isValid(Move m) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Move makeMove(Piece p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Move makeCaptureMove(Piece p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Move> generatePossibleMoves(int[][] board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Move> generatePossibleCaptures(int[][] board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
