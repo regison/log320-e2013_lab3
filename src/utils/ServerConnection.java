419910d3c622b1766231fa4e4e16c92955ef8853
regison <reginald.augustin@gmail.com>
2013-06-25 08:53:11 -0700

    [RAU] ajout des méthodes (partielles) pour la validation et génération des moves

--- src/utils/ServerConnection.java
+++ src/utils/ServerConnection.java
@@ -1,5 +1,10 @@
 package utils;
-
+/**
+ * Cette classe représentera la classe de connexion au réseau 
+ * Elle devrait être implementée selon un type SINGLETON
+ * @author Regison
+ *
+ */
 public class ServerConnection {
 
 }
