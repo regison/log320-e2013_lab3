3e5b4f988caa5ed51dacb8b1ed4f0280184d56cf
regison <reginald.augustin@gmail.com>
2013-06-25 09:03:04 -0700

    [RAU] refactoring

--- src/constants/LOAConstants.java
+++ src/constants/LOAConstants.java
@@ -6,6 +6,9 @@ public class LOAConstants {
 	public static final int PIECE_TYPE_BLACK = 2;
 	public static final int PIECE_TYPE_NULL = 0;
 	
+	public static final int BOARD_LEFT_AND_BOTTOM_SIDE = -1;
+	public static final int BOARD_RIGHT_AND_UPPER_SIDE = 9;
+	
 	public static final int BOARD_SIZE_WIDTH = 8;
 	public static final int BOARD_SIZE_HEIGHT = 8;
 	
@@ -15,8 +18,8 @@ public class LOAConstants {
 	public static final int MIN_PLAYER = Integer.MAX_VALUE;
 	public static final int MAX_PLAYER = Integer.MIN_VALUE;		
 
-	public static final int RIGHT = -1;
-	public static final int LEFT = 1;
+	public static final int RIGHT = 1;
+	public static final int LEFT = -1;
 	public static final int DOWN = -1;
 	public static final int UP = 1;
 	
