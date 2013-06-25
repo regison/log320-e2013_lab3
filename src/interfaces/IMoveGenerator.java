2eafd91b6023341a2970b28e5a98a55f91695e68
regison <reginald.augustin@gmail.com>
2013-06-25 08:52:32 -0700

    [RAU] ajout des méthodes (partielles) pour la validation et génération des moves

--- src/interfaces/IMoveGenerator.java
+++ src/interfaces/IMoveGenerator.java
@@ -1,6 +1,40 @@
 package interfaces;
 
+import java.util.ArrayList;
+
+import model.*;
+
 public interface IMoveGenerator {
 
+	/**
+	 * 
+	 * @param m
+	 * @return
+	 */
+	boolean isValid(Move m);
+	/**
+	 * 
+	 * @param p
+	 * @return
+	 */
+	Move makeMove(Piece p);
+	/**
+	 * 
+	 * @param p
+	 * @return
+	 */
+	Move makeCaptureMove(Piece p);
+	/**
+	 * 
+	 * @param board
+	 * @return
+	 */
+	ArrayList<Move> generatePossibleMoves(int[][]board);
+	/**
+	 * 
+	 * @param board
+	 * @return
+	 */
+	ArrayList<Move> generatePossibleCaptures(int [][]board);
 	
 }
